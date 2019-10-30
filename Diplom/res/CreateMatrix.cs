c39cd9bd440faa183cfcde354fd8147f228a1efb
Black_Empties <Black_Empties@DESKTOP-RJREA88>
2019-10-30 20:57:07 +0200

    create createX1 in CreatMatrix

--- Diplom/res/CreateMatrix.cs
+++ Diplom/res/CreateMatrix.cs
@@ -13,7 +13,13 @@ namespace Diplom.res
             Form1 form1 = new Form1();
             Functions functions = new Functions();
             double[,] matrix = new double[form1.sourseData.Length, form1.verticalLength];
-            // tyt budet for
+            for (int i = 0; i < form1.sourseData.Length;i++)
+            {
+                for ( int j = 0; j < form1.verticalLength;j++) {
+                    matrix[i, j] = functions.getRandom(80, 100);
+                }    
+            }
+
             return matrix;
         }
         public double[,] createX2()
@@ -21,7 +27,7 @@ namespace Diplom.res
             Form1 form1 = new Form1();
             Functions functions = new Functions();
             double[,] matrix = new double[form1.sourseData.Length, form1.verticalLength];
-
+          
             return matrix;
         }
         public double[,] createX3()
@@ -29,7 +35,7 @@ namespace Diplom.res
             Form1 form1 = new Form1();
             Functions functions = new Functions();
             double[,] matrix = new double[form1.sourseData.Length, form1.verticalLength];
-
+        
             return matrix;
         }
         public double[,] createX4()
@@ -37,7 +43,7 @@ namespace Diplom.res
             Form1 form1 = new Form1();
             Functions functions = new Functions();
             double[,] matrix = new double[form1.sourseData.Length, form1.verticalLength];
-
+          
             return matrix;
         }
         public double[,] createX5()
@@ -45,7 +51,7 @@ namespace Diplom.res
             Form1 form1 = new Form1();
             Functions functions = new Functions();
             double[,] matrix = new double[form1.sourseData.Length, form1.verticalLength];
-
+          
             return matrix;
         }
     }
