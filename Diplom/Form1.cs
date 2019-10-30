80305d8e51c6947ae9ed0ff586d99b14bd5319ff
Денисенко Алексей <antimir1996@gmail.com>
2019-10-30 16:13:04 +0200

    Добавьте файлы проекта.

---
+++ Diplom/Form1.cs
@@ -0,0 +1,71 @@
+﻿using Diplom.res;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Diplom
+{
+    public partial class Form1 : Form
+    {
+
+        public double[,] classX1;
+        public double[,] classX2;
+        public double[,] classX3;
+        public double[,] classX4;
+        public double[,] classX5;
+        public int verticalLength = 40;
+        public double[] sourseData = new double[120];
+
+        Functions functions = new Functions();
+        UsingFiles usingFiles = new UsingFiles();
+        MatrixX1 matrixX1 = new MatrixX1();
+        MatrixX2 matrixX2 = new MatrixX2();
+        MatrixX3 matrixX3 = new MatrixX3();
+        MatrixX4 matrixX4 = new MatrixX4();
+        MatrixX5 matrixX5 = new MatrixX5();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void callCreateWriteClasses()
+        {
+            classX1 = matrixX1.create(); 
+            classX2 = matrixX2.create(); 
+            classX3 = matrixX3.create();
+            classX4 = matrixX4.create();
+            classX5 = matrixX5.create();
+            usingFiles.writeFile(classX1, "classX1");
+            usingFiles.writeFile(classX2, "classX2");
+            usingFiles.writeFile(classX3, "classX3");
+            usingFiles.writeFile(classX4, "classX4");
+            usingFiles.writeFile(classX5, "classX5");
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(usingFiles.url + "classX1" + usingFiles.format) && File.Exists(usingFiles.url + "classX2" + usingFiles.format) && File.Exists(usingFiles.url + "classX3" + usingFiles.format) && File.Exists(usingFiles.url + "classX4" + usingFiles.format) && File.Exists(usingFiles.url + "classX5" + usingFiles.format))
+            {
+                classX1 = usingFiles.readFile("classX1"); 
+                classX2 = usingFiles.readFile("classX2"); 
+                classX3 = usingFiles.readFile("classX3");
+                classX4 = usingFiles.readFile("classX4");
+                classX5 = usingFiles.readFile("classX5");
+            }
+
+            else
+            {
+                callCreateWriteClasses();
+            }
+        }
+    }
+}
---
+++ Diplom/res/Functions.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplom.res
+{
+    class Functions
+    {
+        Random rand = new Random();
+
+        public double[] findMean(double[,] Matrix)
+        {
+            double z = 0;
+            double[] meanMas = new double[Matrix.GetLength(0)];
+
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            { //124
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                { //40
+                    z = z + Matrix[i, j];
+                }
+                meanMas[i] = z / 40d;
+                z = 0;
+            }
+            return meanMas;
+        }
+
+        public double[] findLimit(double[] array, string which__one, double delta)
+        { //поиск верхнего, нижнего допуска
+            double[] retArray = new double[array.Length];
+            if (which__one == "Up")
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    retArray[i] = array[i] + delta;
+                }
+            }
+            if (which__one == "Down")
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    retArray[i] = array[i] - delta;
+                }
+            }
+            return retArray;
+        }
+
+        public double[,] BinMatrix(double[,] matrix, double[] dopuskDownn, double[] dopuskUpp)
+        { //Перевод в бинарную матрицу
+            double[,] Bin = new double[matrix.GetLength(0), matrix.GetLength(1)];
+
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+
+                    if (matrix[i, j] < dopuskUpp[i] && matrix[i, j] > dopuskDownn[i])
+                    {
+                        Bin[i, j] = 1d;
+                    }
+                    else
+                    {
+                        Bin[i, j] = 0d;
+                    }
+                }
+
+            }
+            return Bin;
+        }
+
+        public double[] countTrue(double[,] matrix)
+        { // количестов 1 в столбике
+            double[] mas = new double[matrix.GetLength(0)];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == 1)
+                    {
+                        mas[i]++;
+                    }
+                }
+            }
+            return mas;
+        }
+
+        public double getRandom(double from, double to)
+        {
+            return Math.Round(rand.NextDouble() * (to - from) + from, 0);
+        }
+
+        public int getRandom(int from, int to)
+        {
+            return rand.Next(from, to);
+        }
+
+        public double[] FindEtalVecBin(double[] array)
+        {
+            double[] newMeanBin = new double[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0.5d)
+                {
+                    newMeanBin[i] = 1;
+                }
+
+                else
+                {
+                    newMeanBin[i] = 0;
+                }
+            }
+            return newMeanBin;
+        }
+
+        public double findCountXOR(double[] mainEtal, double[] AnotherEtal)
+        {
+            double count = 0;
+            for (int i = 0; i < mainEtal.Length; i++)
+            {
+                if (mainEtal[i] != AnotherEtal[i])
+                {
+                    count = count + 1;
+                }
+            }
+
+            return count;
+        }
+
+        public double FindMaxCount(double[] arr)
+        {
+            double max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+
+        public double FindMinCount(double[] arr)
+        {
+            double min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+
+        public double[] findCountXORforEachLinesMatrix(double[] mainEtal, double[,] bin)
+        {
+            double[] array = new double[bin.GetLength(1)];
+            int sum;
+            for (int j = 0; j < bin.GetLength(1); j++)
+            { //40
+                sum = 0;
+                for (int i = 0; i < bin.GetLength(0); i++)
+                { //124
+                    if (mainEtal[i] != bin[i, j])
+                    {
+                        sum++;
+                    }
+                }
+                array[j] = sum;
+            }
+            return array;
+        }
+
+        public double[] findK(double[] array, int length)
+        {
+            double[] k = new double[length];
+            for (int i = 0; i < length; i++)
+            {
+                double tmp = 0;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[j] <= i)
+                    {
+                        tmp++;
+                    }
+                }
+                k[i] = tmp / array.Length;
+            }
+            return k;
+        }
+
+        public double[] find_KFE(double[] k1, double[] k2, int length)
+        {
+            double[] E = new double[length];
+            double[] pt = new double[length];
+            double[] pf = new double[length];
+            double[] sump = new double[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                pt[i] = ((k1[i] + 1 - k2[i]) / 2);
+                pf[i] = ((1 - k1[i] + k2[i]) / 2);
+                sump[i] = pt[i] - pf[i];
+                E[i] = Math.Log((pt[i] + 0.005) / (pf[i] + 0.005), 2.0) * (sump[i]);
+            }
+
+            return E;
+        }
+
+        public int findWorkArea(double[] k1, double[] k2)
+        {
+            int radius = 0;
+            for (int i = 0; i < k2.Length; i++)
+            {
+                if (k1[i] >= 0.5 && k2[i] < 0.5)
+                {
+                    radius++;
+                }
+            }
+
+            return radius;
+        }
+
+        public double isFindWorkArea(double[] E, double E_max, double[] k, int radius)
+        {
+            double k_mid = 0;
+            for (int i = 0; i < k.Length; i++)
+            {
+                if (radius != 0)
+                {
+                    if (E[i] == E_max)
+                    {
+                        k_mid = k[i];
+                    }
+                }
+                else
+                {
+                    k_mid = k[i] + double.PositiveInfinity;
+                }
+            }
+            return k_mid;
+        }
+    }
+}
